7ca60f0ecec48c22156ab2ac94103ddbc80d6397
thunsirikorn <thunsirikorn.n@gmail.com>
2021-03-04 16:12:17 +0700

    angular3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BackendService } from './backend.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'back-office';
+
+  constructor(private backendService: BackendService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,94 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { MatCardModule } from '@angular/material/card';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { NgMaterialMultilevelMenuModule } from 'ng-material-multilevel-menu';
+import { BackendService } from './backend.service';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+
+
+const routes: Routes = [
+  {
+  path: '',
+  component: DashboardComponent
+  },
+  {
+  path: 'dashboard',
+  component: DashboardComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+    },
+  {
+  path: 'products',
+  component: ProductsComponent
+  },
+  {
+  path: '**',
+  component: PageNotFoundComponent
+  }
+  ];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent,
+    FooterComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatListModule,
+    MatGridListModule,
+    CdkTreeModule,
+    MatCardModule,
+    FontAwesomeModule,
+    MatExpansionModule,
+    NgMaterialMultilevelMenuModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [BackendService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+ }
